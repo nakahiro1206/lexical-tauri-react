2882d5e4b9a9d82fe9d24586a4ac0113b201781d
nakahiro1206 <32neteru-56neteru-dullnoon@g.ecc.u-tokyo.ac.jp>
2025-02-16 02:40:17 +0900

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import "../styles/styles.css";
+import { Editor } from "@/components/TextEditor";
+import { TypingAnimation } from "@/components/TypingAnimation";
+
+export default function App() {
+  return (
+    <div className="App">
+      aaaaaa
+      <TypingAnimation />
+      <Editor />
+    </div>
+  );
+}
